bab51c607d1a6c3e10a57d05284d23ad59eb6d9b
Antti_Ojala <antti.i.ojala@helsinki.fi>
2020-11-16 23:56:16 +0200

    tehtävät 4-10 tehty

--- viikko3/tehtavat4-10/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
+++ viikko3/tehtavat4-10/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
@@ -25,19 +25,47 @@ public class AuthenticationService {
 
     public CreationStatus createUser(String username, String password, String passwordConfirmation) {
         CreationStatus status = new CreationStatus();
-        
+
         if (userDao.findByName(username) != null) {
             status.addError("username is already taken");
         }
 
-        if (username.length()<3 ) {
+        if (username.length() < 3) {
             status.addError("username should have at least 3 characters");
+        }
+
+        if (password.length() < 8) {
+            status.addError("password should have at least 8 characters");
+        }
+
+        if (!password.equals(passwordConfirmation)) {
+            status.addError("password and password confirmation do not match");
+        }
+
+        for (int i = 0; i < username.length(); i++) {
+            int merkki = username.charAt(i);
+            if (merkki < 97 || merkki > 122) {
+                status.addError("username should only contain lowercase characters from a to z");
+                break;
+            }
+        }
+        
+        boolean passwordError = true;
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetter(password.charAt(i))) {
+                passwordError = false;
+                break;
+            }
+        }
+        
+        if (passwordError) {
+            status.addError("password should not contain only letters");
         }
 
         if (status.isOk()) {
             userDao.add(new User(username, password));
         }
-        
+
         return status;
     }
 
---
+++ viikko3/tehtavat4-10/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -0,0 +1,148 @@
+package ohtu;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class Stepdefs {
+    //WebDriver driver = new ChromeDriver();
+    WebDriver driver = new HtmlUnitDriver();
+    String baseUrl = "http://localhost:4567";
+    
+    @Given("login is selected")
+    public void loginIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("login"));       
+        element.click();   
+    }    
+    
+    @When("correct username {string} and password {string} are given")
+    public void correctUsernameAndPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }    
+    
+    @Then("user is logged in")
+    public void userIsLoggedIn() {
+        pageHasContent("Ohtu Application main page");
+    }    
+ 
+    @When("correct username {string} and incorrect password {string} are given")
+    public void correctUsernameAndIncorrectPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }    
+    
+    @Then("user is not logged in and error message is given")
+    public void userIsNotLoggedInAndErrorMessageIsGiven() {
+        pageHasContent("invalid username or password");
+        pageHasContent("Give your credentials to login");
+    }     
+    
+    @When("nonexistent username {string} and password {string} are given")
+    public void nonExistentUsernameIsGiven(String username, String password) {
+        logInWith(username, password);
+    }
+    
+    @Given("command new user is selected")
+    public void registerNewUserIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+    }
+    
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void validUsernamePasswordAndConfirmationEntered(String username, String password) {
+        registerWith(username, password, password);
+    }
+    
+    @Then("a new user is created")
+    public void newUserIsCreated() {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+    
+    @When("too short username {string} and valid password {string} and matching password confirmation are entered")
+    public void tooShortUsernameAndValidPasswordAndConfirmationEntered(String username, String password) {
+        registerWith(username, password, password);
+    }
+    
+    @Then("user is not created and error \"username should have at least 3 characters\" is reported")
+    public void userNotCreatedAndTooShortUsernameErrorMessageIsGiven() {
+        pageHasContent("username should have at least 3 characters");
+    }
+    
+    @When("valid username {string} and too short password {string} and matching password confirmation are entered")
+    public void validUsernameAndTooShortPasswordAndConfirmationEntered(String username, String password) {
+        registerWith(username, password, password);
+    }
+    
+    @Then("user is not created and error \"password should have at least 8 characters\" is reported")
+    public void userNotCreatedAndTooShortPasswordErrorMessageIsGiven() {
+        pageHasContent("password should have at least 8 characters");
+    }
+    
+    @When("valid username {string} and password {string} and non-matching password confirmation {string} are entered")
+    public void validUsernameAndPasswordAndNonMatchingConfirmationEntered(String username, String password, String nPassword) {
+        registerWith(username, password, nPassword);
+    }
+    
+    @Then("user is not created and error \"password and password confirmation do not match\" is reported")
+    public void userNotCreatedAndNonMatchingConfirmationErrorIsGiven() {
+        pageHasContent("password and password confirmation do not match");
+    }
+    
+    @Given("user with username {string} with password {string} is successfully created")
+    public void userWithUserNameAndPasswordSuccesfullyCreated(String username, String password) {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+        registerWith(username, password, password);
+        proceedToLogout();     
+    }
+    
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+        
+    /* helper methods */
+ 
+    private void pageHasContent(String content) {
+        assertTrue(driver.getPageSource().contains(content));
+    }
+        
+    private void logInWith(String username, String password) {
+        assertTrue(driver.getPageSource().contains("Give your credentials to login"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("login"));
+        element.submit();  
+    } 
+    
+    private void registerWith(String username, String password, String confPassword) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(confPassword);
+        element = driver.findElement(By.name("signup"));
+        element.submit();  
+    }
+    
+    private void proceedToLogout() {
+        assertTrue(driver.getPageSource().contains("Welcome to Ohtu Application!"));
+        WebElement element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+    }
+}
